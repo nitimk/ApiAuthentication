5b0a38b1fec5f166ccefd921bce5585a478e38db
Niti <nitimk@gmail.com>
2021-09-22 16:29:36 -0400

    database connection

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/register',(req, res) => {
-    res.send('Register');
+//VALIDATION
+const Joi = require('@hapi/joi');
+
+
+const Schema = {
+    name: Joi.string().min(6).required(),
+    email:Joi.string().min(6).required().
+}
+
+
+router.post('/register', async(req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+       res.status(400).send(err); 
+    }
 });
-
 
 
 
